a9bc972f40a9dca2d7275d183567dfffbd2dd8c3
Simon <sdanisch@gmail.com>
2018-07-12 18:19:33 +0200

    Update build_tarballs.jl

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -40,16 +40,8 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    BinaryProvider.Linux(:i686, :glibc),
-    BinaryProvider.Linux(:x86_64, :glibc),
-    BinaryProvider.Linux(:aarch64, :glibc),
-    BinaryProvider.Linux(:armv7l, :glibc),
-    BinaryProvider.Linux(:powerpc64le, :glibc),
-    BinaryProvider.MacOS(),
-    BinaryProvider.Windows(:i686),
-    BinaryProvider.Windows(:x86_64)
-]
+platforms = BinaryBuilder.supported_platforms()
+
 
 # The products that we will ensure are always built
 products(prefix) = [
@@ -61,4 +53,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, "Pixman", sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, "Pixman", v"0.34.0", sources, script, platforms, products, dependencies)
